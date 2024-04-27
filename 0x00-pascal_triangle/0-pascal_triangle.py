0f0b837031feaa160f1eaede2cb43f8b2979fff9
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-04-27 22:55:31 +0000

    PEP8 Standard

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 def pascal_triangle(n):
     """
-    Returns a list of lists representing the Pascal's triangle of n.
+    Returns a list of lists representing Pascal's triangle up to the nth row.
     Returns an empty list if n <= 0.
-    
+
     Parameters:
         n (int): Number of rows for Pascal's triangle.
-        
+
     Returns:
         list of lists: Pascal's triangle represented as a list of lists.
     """
@@ -26,9 +25,6 @@ def pascal_triangle(n):
 
     return triangle
 
-# Print the Pascal's triangle up to n = 10. n = 0, n = 1, n = 5, n = 100
-print(pascal_triangle(5))
-print(pascal_triangle(1))
-print(pascal_triangle(0))
+
+# Print Pascal's triangle up to n = 10 (for illustration purposes)
 print(pascal_triangle(10))
-print(pascal_triangle(100))
