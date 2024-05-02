4b669cf93b133e9be4fa2d4f80c524db9b9d8b98
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-05-02 23:02:36 +0000

    pycodestyle 0x01-lockboxes/0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
+"""A method that determines if all the boxes can be opened."""
 
-"""A method that determines if all boxes can be opened."""
 
-
-def can_unlock_all(boxes):
-    """This function takes a list of lists, where the content of a list
-    unlocks other lists.
-
-    Args:
-        boxes: A list of lists representing the boxes and their keys.
-
-    Returns:
-        True if all boxes can be unlocked, False otherwise.
+def canUnlockAll(boxes):
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
 
     keys = [0]
     for key in keys:
-        for box_key in boxes[key]:
-            if box_key not in keys and box_key < len(boxes):
-                keys.append(box_key)
-    return len(keys) == len(boxes)
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    return len(keys) == len(boxes)  # More concise check
 
-
-# Example usage
-boxes = [[1], [2], [1, 3], [3]]
-print(can_unlock_all(boxes))  # Output: True
-
-boxes = [[1], [2], [3], []]
-print(can_unlock_all(boxes))  # Output: False
+# Remove the blank line with whitespace (if your editor adds it)
