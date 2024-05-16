240fa42036528747efe159e879d3a2fc2d125947
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-05-17 00:06:50 +0100

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -6,12 +6,9 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    # Separate arguments for better readability
-    log_line = "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}".format(
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
         datetime.datetime.now(),
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
-    )
-    sys.stdout.write(log_line + "\n")
-    sys.stdout.flush()
+    ))
