98f16348130277f0cbccdf520bc87679ad9efa3b
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-05-17 00:22:44 +0100

    0-stats.py 103

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,66 @@
 #!/usr/bin/python3
 """
-module contains a script that reads stdin line by line and computes metrics
+Write a script that reads stdin line by line and computes metrics
 """
+import random
 import sys
+from time import sleep
+import datetime
 
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+def print_stats(file_size, status_codes):
+    """
+    Print the statistics
+    """
+    for i in range(10000):
+        sleep(random.random())
+        sys.stdout.write(
+            "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n"
+            .format(
+                random.randint(1, 255), random.randint(1, 255),
+                random.randint(1, 255), random.randint(1, 255),
+                datetime.datetime.now(),
+                random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+                random.randint(1, 1024)))
+        sys.stdout.flush()
 
-file_size = 0
 
+def main():
+    """
+    Main function
+    """
+    file_size = 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    count = 0
+    stats = {k: 0 for k in status_codes}
 
-def print_metrics():
-    """prints of the logs"""
-    print("File size: {}".format(file_size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
+    def print_statistics():
+        print("File size: {:d}".format(file_size))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                sc = data[-2]
+                if sc in stats:
+                    stats[sc] += 1
+            except (IndexError, KeyError):
+                pass
+            try:
+                file_size += int(data[-1])
+            except (TypeError, IndexError, ValueError):
+                pass
+            if count % 10 == 0:
+                print_statistics()
+        print_statistics()
+    except KeyboardInterrupt:
+        print_statistics()
+        raise
 
 
 if __name__ == "__main__":
-    count = 0
-    try:
-        for line in sys.stdin:
-            try:
-                elems = line.split()
-                file_size += int(elems[-1])
-                if elems[-2] in status_codes:
-                    status_codes[elems[-2]] += 1
-            except Exception:
-                pass
-            if count == 9:
-                print_metrics()
-                count = -1
-            count += 1
-    except KeyboardInterrupt:
-        print_metrics()
-        raise
-    print_metrics()
+    main()
