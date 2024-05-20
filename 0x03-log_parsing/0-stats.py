32350c6f732c30497c811a9326a3e314b7c01341
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-05-20 04:14:03 +0100

    Update 0-stats.py new 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python3
 """
-Write a script that reads stdin line by line and computes metrics
+Script that reads stdin line by line and computes metrics
 """
-import random
 import sys
-from time import sleep
-import datetime
-
-
-def print_stats(file_size, status_codes):
-    """
-    Print the statistics
-    """
-    for i in range(10000):
-        sleep(random.random())
-        sys.stdout.write(
-            "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n"
-            .format(
-                random.randint(1, 255), random.randint(1, 255),
-                random.randint(1, 255), random.randint(1, 255),
-                datetime.datetime.now(),
-                random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-                random.randint(1, 1024)))
-        sys.stdout.flush()
 
 
 def main():
@@ -30,11 +10,14 @@ def main():
     Main function
     """
     file_size = 0
-    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     count = 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in status_codes}
 
     def print_statistics():
+        """
+        Print the accumulated statistics
+        """
         print("File size: {:d}".format(file_size))
         for k, v in sorted(stats.items()):
             if v:
@@ -45,18 +28,23 @@ def main():
             count += 1
             data = line.split()
             try:
+                # Assuming the status code is the second-to-last element
                 sc = data[-2]
                 if sc in stats:
                     stats[sc] += 1
             except (IndexError, KeyError):
                 pass
             try:
+                # Assuming the file size is the last element
                 file_size += int(data[-1])
-            except (TypeError, IndexError, ValueError):
+            except (IndexError, ValueError):
                 pass
+
             if count % 10 == 0:
                 print_statistics()
+
         print_statistics()
+
     except KeyboardInterrupt:
         print_statistics()
         raise
