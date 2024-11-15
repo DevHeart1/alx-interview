96aa8455d8a0440386d09bf0aecf28977c8aaa47
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-11-15 04:27:50 +0000

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,31 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-// Convert request to promise-based function
-function makeRequest(url) {
+function makeRequest (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
-      if (error) reject(error);
-      resolve(JSON.parse(body));
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
   });
 }
 
-// Main function to get and display characters
-async function getMovieCharacters(movieId) {
+async function getMovieCharacters (movieId) {
   try {
-    // Get movie data
     const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
     const movieData = await makeRequest(movieUrl);
 
-    // Get all characters data (maintaining order)
     const characterPromises = movieData.characters.map(url => makeRequest(url));
     const characters = await Promise.all(characterPromises);
 
-    // Print character names in order
     characters.forEach(character => console.log(character.name));
   } catch (error) {
-    console.error('Error:', error.message);
-    process.exit(1);
+    console.error(error);
   }
 }
 
-// Get movie ID from command line argument
 const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a movie ID');
-  process.exit(1);
-}
-
 getMovieCharacters(movieId);
